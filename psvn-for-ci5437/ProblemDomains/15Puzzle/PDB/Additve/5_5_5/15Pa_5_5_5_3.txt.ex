d5929fd360918002db4bbc89cfb3b8c3226150b9
Dan <initial-mockingbird@gmail.com>
2023-06-11 22:57:46 -0400

    Some results

---
+++ psvn-for-ci5437/ProblemDomains/15Puzzle/PDB/Additve/5_5_5/15Pa_5_5_5_3.txt.ex
@@ -0,0 +1,21 @@
+map tile 1 b
+map tile 2 b
+map tile 3 b
+map tile 4 b
+map tile 5 b
+map tile 6 b
+map tile 7 b
+map tile 8 b
+map tile 9 9
+map tile 10 10
+map tile 11 b
+map tile 12 b
+map tile 13 13
+map tile 14 14
+map tile 15 15
+map tile b b
+
+# ZZ ZZ ZZ ZZ
+# ZZ ZZ ZZ ZZ
+# 09 10 ZZ ZZ
+# 13 14 15 16
