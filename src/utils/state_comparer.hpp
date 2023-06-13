dbcd23546e97a4dc3bd5b6d9e35fd303c0b7270f
xdgorgola <pedro18fe@gmail.com>
2023-06-12 23:48:26 -0400

    4-18 hanoi ida

--- src/utils/state_comparer.hpp
+++ src/utils/state_comparer.hpp
@@ -4,7 +4,8 @@ bool operator==(const state_t &a, const state_t &b) {
     return compare_states(&a, &b) == 0;
 }
 
-struct hash<state_t>
+template <>
+struct std::hash<state_t>
 {
   std::size_t operator()(const state_t& state) const
   {
